8bfa9d8c15685c4fa08e3aeff96622c398f5f33e
chenmingzhang <cmzhang1318@hotmail.com>
2017-03-05 23:01:14 +1000

    add index and gec icon

--- area51/plot.js
+++ area51/plot.js
@@ -64,63 +64,6 @@
           .y(function(d) { return y_temp(d[key_temp[i]]); });
   })
   
-  // ----------------------draw picture for the entire setup ---------------------
-  var svg_pic = d3.select("body")
-       .append("svg")
-       .attr("width", 1280)
-       .attr("height", 1124)
-       .style("border", "0px solid black");
-  
-  var text = svg_pic.selectAll("text")
-       .data([0])
-       .enter()
-       .append("text")
-       .text("Experimental Setup")
-       .style("font-size", "25px")
-       .attr("x", "0")
-       .attr("y", "20");
-  
-   var imgs = svg_pic.selectAll("image").data([0]);
-       imgs.enter()
-       .append("svg:image")
-       .attr("xlink:href", "https://outbox.eait.uq.edu.au/uqczhan2/photo_geo_1st/setup.PNG")
-       .attr("x", "00")
-       .attr("y", "30")
-       .attr("width", "1280")
-       .attr("height", "1024");
-  
-  
-  // ----------------------below is to draw picture for the entire setup ---------------------
-  var svg_pic_realtime = d3.select("body")
-       .append("svg")
-       .attr("width", 1280)
-       .attr("height", 1124)
-       .style("border", "0px solid black");
-  
-  var text = svg_pic_realtime.selectAll("text")
-       .data([0])
-       .enter()
-       .append("text")
-       .text("Real-time snapshot of tailings surface")
-       .style("font-size", "25px")
-       .attr("x", "0")
-       .attr("y", "20");
-  
-   var imgs = svg_pic_realtime.selectAll("image").data([0]);
-       imgs.enter()
-       .append("svg:image")
-       .attr("xlink:href", "https://outbox.eait.uq.edu.au/uqczhan2/photo_geo_1st/2017_02_09_12_58_geo_1st.jpg")
-       .attr("x", "00")
-       .attr("y", "50")
-       .attr("width", "1280")
-       .attr("height", "924");
-  
-  	   svg_pic_realtime.append("text")
-                  .text("Real-time results")
-                  .style("font-size", "25px")
-                  .attr("x", "0")
-                  .attr("y", "1100");
-  // ----------------------above is to draw picture for the entire setup ---------------------
   
   // --------------Adds the svg canvas  -------------------
   //  2017-03-02 12:08  this determines the sequence of plotting appreas.
