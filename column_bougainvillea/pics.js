84c6d82ca95bb8423a8b4b061f9613b8a7d9532f
diablo465 <cmzhang1318@hotmail.com>
2017-07-12 18:01:25 +1000

    now fixed

--- column_bougainvillea/pics.js
+++ column_bougainvillea/pics.js
@@ -50,9 +50,28 @@ var imgs = svg_pic_realtime.selectAll("image").data([0]);
      .attr("width", "1280")
      .attr("height", "924");
 
-     svg_pic_realtime.append("text")
-            .text("Real-time results")
-            .style("font-size", "25px")
-            .attr("x", "0")
-            .attr("y", "1100");
-// ----------------------above is to draw picture for the entire setup ---------------------
+// ----------------------below is to draw picture for the entire setup ---------------------
+var svg_pic_actual = d3.select("#blockDescription")
+     .append("svg")
+     .attr("width", 1280)
+     .attr("height", 1124)
+     .style("border", "0px solid black");
+
+var text = svg_pic_actual.selectAll("text")
+     .data([0])
+     .enter()
+     .append("text")
+     .text("Real-time results")
+     .style("font-size", "25px")
+     .attr("x", "0")
+     .attr("y", "20");
+
+var imgs = svg_pic_actual.selectAll("image").data([0]);
+     imgs.enter()
+     .append("svg:image")
+     .attr("xlink:href", "https://outbox.eait.uq.edu.au/uqczhan2/bougainvillea/latest.jpg")
+     .attr("x", "00")
+     .attr("y", "50")
+     .attr("width", "1280")
+     .attr("height", "924");
+
